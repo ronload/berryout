f7e90d969a51051fa48e3bb7a3e4257ce6264db9
ronload <91997734+ronload@users.noreply.github.com>
2026-03-14 02:11:14 +0800

    feat(ui): add grid pattern background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
 import { Header } from "@/components/header";
 import { CountdownDisplay } from "@/components/countdown-display";
-
+import { GridPattern } from "@/components/grid-pattern";
+import { cn } from "@/lib/utils";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
+      <GridPattern
+        x={500}
+        y={400}
+        squares={[
+          [-11, 9],
+          [-9, 8],
+          [-8, 10],
+          [-10, 4],
+          [-7, 11],
+          [-6, 9],
+          [-4, 12],
+          [4, 4],
+          [5, 1],
+          [8, 2],
+          [5, 3],
+          [5, 5],
+          [10, 10],
+          [12, 15],
+          [15, 10],
+          [10, 15],
+          [14, 12],
+          [11, 16],
+          [16, 8],
+        ]}
+        className={cn(
+          "[mask-image:radial-gradient(250px_circle_at_center,white,transparent)]",
+          "sm:[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+          "md:[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-40%] h-[200%] skew-y-12",
+        )}
+      />
       <Header />
       <main className="flex flex-1 justify-center px-4 py-8 pb-[max(2rem,env(safe-area-inset-bottom))] w-full">
         <CountdownDisplay />
